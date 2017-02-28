be6dca09b2c526145d70160e109f71d078143d42
mark kannengießer <dev@shop-base.de>
2017-02-28 18:31:48 +0100

    font-awesome cnd local sources as admin config

--- includes/template_top.php
+++ includes/template_top.php
@@ -31,7 +31,10 @@
 
 <link href="ext/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <!-- font awesome -->
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+<?php $font_awesome_css = USE_EXTERNAL_CND === 'true' ? 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css' :  
+	'ext/font-awesome/4.6.3/css/font-awesome.min.css';
+?>
+<link href="<?php echo $font_awesome_css; ?>" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
 
 <link href="custom.css" rel="stylesheet">
 <link href="user.css" rel="stylesheet">
